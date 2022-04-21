0cc48b6a5ab9ada1c70e9fdbf493aaa848e4658c
wilson cely <wectcorp@gmail.com>
2022-04-21 19:44:58 +0200

    back structure ended

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+puts 'Cleaning database'
+Flat.destroy_all
+
+puts 'Creating  flats'
+
+flat1 = Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+puts "Created #{flat1.name}"
+
+flat2 = Flat.create!(
+  name: 'Stylish House Close To Thames River',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas.',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+puts "Created #{flat2.name}"
+
+flat3 = Flat.create!(
+  name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
+  price_per_night: 110,
+  number_of_guests: 2
+)
+
+flat4 = Flat.create!(
+  name: 'Amazing house in the 14eme district',
+  address: '34 rue raymond losserand 75014 Paris',
+  description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
+  price_per_night: 120,
+  number_of_guests: 3
+)
+
+
+puts "Created #{flat4.name}"
